6e461099beab004189fd7bab0dae437006cfdf32
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-28 16:02:07 +0530

    [Soubarnika]Performing basic file IO operations

---
+++ FileIO Operations/BasicOperation.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileIO_Operations
+{
+    class BasicOperation
+    {
+        public static void FileExists(string filepath)
+        {
+            if (File.Exists(filepath))
+            {
+                Console.WriteLine("File exists in the given path");
+            }
+            else
+            {
+                Console.WriteLine("File does not exists");
+            }
+        }
+        //reading the file line by line
+       public static void ReadAllLine(string filepath)
+        {
+            //storing the each line in array
+            string[] lines;
+            lines = File.ReadAllLines(filepath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+        }
+        //reading all the line as single string
+        public static void ReadFile(string filePath)
+        {
+            string text;
+            text = File.ReadAllText(filePath);
+            Console.WriteLine("{0}", text);
+        }
+        //make the copy of the text file
+        public static void CopyFile(string InputFile, string OutputFile)
+        {
+            File.Copy(InputFile, OutputFile);
+            Console.WriteLine("File is copied");
+        }
+        //Deleting the particular file
+        public static void DeleteFile(string filePath)
+        {
+            File.Delete(filePath);
+            Console.WriteLine("File is deleted");
+        }
+      //  Reading the file using stream reader
+        public static void ReadFromStreamReader(string filePath)
+        {
+            Console.WriteLine("Reading text using file reader");
+            StreamReader reader = File.OpenText(filePath);
+            string text = "";
+            while ((text = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(text);
+            }
+            reader.Close();
+        }
+        //Writing into the file using stream reader
+        public static void WriteUsingStreamWriter(string filePath)
+        {
+            Console.WriteLine("Writing text into file");
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine("Writing into the file");
+                writer.Close();
+            }
+        }
+    }
+}
--- FileIO Operations/Program.cs
+++ FileIO Operations/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileIO_Operations
 {
@@ -10,6 +11,17 @@ namespace FileIO_Operations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Implementation of File IO operations");
+            string filepath = @"C:\Users\hp\source\repos\FileIO Operations\FileIO Operations\TextFile1.txt";
+            string outputFile = @"C:\Users\hp\Desktop\Output.txt";
+            BasicOperation.FileExists(filepath);
+            BasicOperation.ReadAllLine(filepath);
+            BasicOperation.ReadFile(filepath);
+           // BasicOperation.CopyFile(filepath,outputFile);
+            BasicOperation.ReadFromStreamReader(filepath);
+            BasicOperation.WriteUsingStreamWriter(filepath);
+           
+            Console.Read();
         }
     }
 }
